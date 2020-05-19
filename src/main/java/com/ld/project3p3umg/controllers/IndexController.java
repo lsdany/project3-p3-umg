6c2f020423146d4c9ad438b6253dc4114f689703
luisdany <luisdany@gmail.com>
2020-05-19 06:15:04 -0600

    views

--- src/main/java/com/ld/project3p3umg/controllers/IndexController.java
+++ src/main/java/com/ld/project3p3umg/controllers/IndexController.java
@@ -1,8 +1,24 @@
 package com.ld.project3p3umg.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * @author luisdany
  */
 
+@Controller
 public class IndexController {
+
+    @GetMapping({"/index", "", "/"})
+    public String indexView(Model model){
+        return "index";
+    }
+
+
+    @GetMapping("/f2")
+    public String f2(Model model){
+        return "f2";
+    }
 }
